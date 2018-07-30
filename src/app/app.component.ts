a14086db17543ce584538fe8eb0ad41949267210
William Molina <wondercodesoft@gmail.com>
2018-07-29 21:34:58 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { BrowserWindow } from 'electron';
+const composerize = (<any>window).require("composerize-plus");
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-electron';
+  lista;
+  window: BrowserWindow;
+  constructor() {
+    composerize('docker run -p 8080:8080 nginx + 70b3c241efbc')
+  }
+  listarArchivos = function(){
+
+  }
+
 }
